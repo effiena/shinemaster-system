4ae4c902c0d68312b041dd856f538a0f53347d76
Fida <noofy.fyda@icloud.com>
2026-03-08 00:10:57 +0800

    add eventlet in app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, redirect, jsonify, session, url_for
 import sqlite3
 from flask_socketio import SocketIO, emit
