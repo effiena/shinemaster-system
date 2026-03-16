e67d23d1d5e5b94c756407d365f68d104a2554ba
Fida <noofy.fyda@icloud.com>
2026-03-16 22:29:25 +0800

    remove eventlet

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import eventlet
-eventlet.monkey_patch()
+#import eventlet
+#eventlet.monkey_patch()
 
 from flask import Flask, render_template, request, redirect, jsonify, session, url_for, send_file
 import sqlite3
