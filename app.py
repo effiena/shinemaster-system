7a9221ea94d7f87e1b49f96164ff9b45cae4fac4
Fida <noofy.fyda@icloud.com>
2026-03-24 03:16:35 +0800

    add datetime

--- app.py
+++ app.py
@@ -1178,6 +1178,7 @@ def booking():
         timeslots=timeslots,
         plate=plate,
         current_date=current_date_str,
+        datetime=datetime
         booked_times=booked_times,
         disabled_slots=disabled_slots,
         today_date_str=today_date.strftime("%Y-%m-%d")
